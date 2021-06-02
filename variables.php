4106d1da1ac4aa734d5867baf14b9b01cb6bc0f5
Muhammad Essa <iMuhammadessa@gmail.com>
2021-06-02 10:31:31 +0500

    variable file has been created

---
+++ variables.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Local Variables</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+<?php
+
+$name = 'Muhammad Essa <br>';
+$email = 'Contact@muhammadessa.com <br>';
+$age = 23;
+echo "Name: ". $name . "Email: " . $email . "Age: ". $age;
+
+?>
