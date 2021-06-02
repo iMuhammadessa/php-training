dd3b2928f13f71b973fecb85fd42e26743ad58fc
Muhammad Essa <iMuhammadessa@gmail.com>
2021-06-02 10:46:36 +0500

    Array file has been created

---
+++ arrays.php
@@ -0,0 +1,6 @@
+<?php 
+
+$info = array ('Muhammad Essa', 'Contact@muhammadessa.com', 'Male', '23');
+
+echo "Name: ". $info[0]. "<br> Email: ". $info[1]. "<br> Gender: ". $info[2]. "<br> Age: ". $info[3]. "<br>";
+?>
