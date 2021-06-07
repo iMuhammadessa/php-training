ccaeedc93705f14c7e8635c640c54a4cd2a802f9
Muhammad Essa <iMuhammadessa@gmail.com>
2021-06-07 13:05:37 +0500

    IncludeData file created

---
+++ includeData.php
@@ -0,0 +1,12 @@
+<?php
+
+function includeData() {
+    echo $name = "Muhammad Essa <br>";
+    echo $email = "admin@muhammadessa.com <br>";
+    echo $age = 23 ."<br>";
+    echo $gender = "Male";
+}
+
+includeData();
+
+?>
