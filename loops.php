2609304ba28e8c02534fa6e091f711eb8fdef713
Muhammad Essa <iMuhammadessa@gmail.com>
2021-06-07 12:41:29 +0500

    While loop has been used

---
+++ loops.php
@@ -0,0 +1,16 @@
+<?php
+
+/* Loops are used to repeat value again and again
+- Mostly used loops in php
+- for - loop
+- while - loop
+- foreach - loop
+- do while - loop */
+
+$number = 1;
+
+while ($number<=10) {
+    echo $number++ ."<br>";
+}
+
+?>
