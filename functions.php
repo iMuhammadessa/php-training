1c422075d652294c3d06b1083e96e8b6acbb94ca
Muhammad Essa <iMuhammadessa@gmail.com>
2021-06-07 10:40:55 +0500

    functions.php file has been created

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+
+// function userFunction() {
+//     echo    $name = "Muhammad Essa <br>";
+//     echo    $email = "contact@muhammadessa.com <br>";
+//     echo    $age = 23;
+// }
+
+// userFunction();
+
+function arrayFun() {
+    include("functions.html");
+}
+
+arrayFun();
+
+?>
